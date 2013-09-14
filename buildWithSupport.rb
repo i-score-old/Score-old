481c9f29b6f1fb45dc55fd22343dbf147c6389fe
bltzr <pb@zkrx.org>
2013-09-14 13:02:13 +0200

    removing sudo to avoid property problems

--- buildWithSupport.rb
+++ buildWithSupport.rb
@@ -35,33 +35,33 @@ if  win32?
 elsif mac?
     
     # Copy support/jamoma folder into /usr/local/jamoma folder
-    `sudo cp "#{glibdir}"/support/jamoma/extensions/* /usr/local/jamoma/extensions`
-    `sudo cp "#{glibdir}"/support/jamoma/includes/* /usr/local/jamoma/includes`
-    `sudo cp "#{glibdir}"/support/jamoma/lib/* /usr/local/jamoma/lib`
+    `cp "#{glibdir}"/support/jamoma/extensions/* /usr/local/jamoma/extensions`
+    `cp "#{glibdir}"/support/jamoma/includes/* /usr/local/jamoma/includes`
+    `cp "#{glibdir}"/support/jamoma/lib/* /usr/local/jamoma/lib`
     
     # Create alias
-    `sudo ln -s /usr/local/jamoma/lib/JamomaFoundation.dylib /usr/local/lib/JamomaFoundation.dylib`
-    `sudo ln -s /usr/local/jamoma/lib/JamomaDSP.dylib /usr/local/lib/JamomaDSP.dylib`
-    `sudo ln -s /usr/local/jamoma/lib/JamomaModular.dylib /usr/local/lib/JamomaModular.dylib`
+    `ln -s /usr/local/jamoma/lib/JamomaFoundation.dylib /usr/local/lib/JamomaFoundation.dylib`
+    `ln -s /usr/local/jamoma/lib/JamomaDSP.dylib /usr/local/lib/JamomaDSP.dylib`
+    `ln -s /usr/local/jamoma/lib/JamomaModular.dylib /usr/local/lib/JamomaModular.dylib`
     
-    `sudo ln -s /usr/local/jamoma/extensions/AnalysisLib.ttdylib /usr/local/lib/AnalysisLib.ttdylib`
-    `sudo ln -s /usr/local/jamoma/extensions/FunctionLib.ttdylib /usr/local/lib/FunctionLib.ttdylib`
-    `sudo ln -s /usr/local/jamoma/extensions/NetworkLib.ttdylib /usr/local/lib/NetworkLib.ttdylib`
-    `sudo ln -s /usr/local/jamoma/extensions/System.ttdylib /usr/local/lib/System.ttdylib`
-    `sudo ln -s /usr/local/jamoma/extensions/DataspaceLib.ttdylib /usr/local/lib/DataspaceLib.ttdylib`
-    `sudo ln -s /usr/local/jamoma/extensions/Minuit.ttdylib /usr/local/lib/Minuit.ttdylib`
-    `sudo ln -s /usr/local/jamoma/extensions/OSC.ttdylib /usr/local/lib/OSC.ttdylib`
+    `ln -s /usr/local/jamoma/extensions/AnalysisLib.ttdylib /usr/local/lib/AnalysisLib.ttdylib`
+    `ln -s /usr/local/jamoma/extensions/FunctionLib.ttdylib /usr/local/lib/FunctionLib.ttdylib`
+    `ln -s /usr/local/jamoma/extensions/NetworkLib.ttdylib /usr/local/lib/NetworkLib.ttdylib`
+    `ln -s /usr/local/jamoma/extensions/System.ttdylib /usr/local/lib/System.ttdylib`
+    `ln -s /usr/local/jamoma/extensions/DataspaceLib.ttdylib /usr/local/lib/DataspaceLib.ttdylib`
+    `ln -s /usr/local/jamoma/extensions/Minuit.ttdylib /usr/local/lib/Minuit.ttdylib`
+    `ln -s /usr/local/jamoma/extensions/OSC.ttdylib /usr/local/lib/OSC.ttdylib`
     
     # Copy Score headers to include them into other application
     # (except the includes folder because it is done by the support/build.rb script)
-    `sudo cp "#{glibdir}"/library/tests/*.h /usr/local/jamoma/includes`
-    `sudo cp "#{glibdir}"/extensions/TimePluginLib.h /usr/local/jamoma/includes`
+    `cp "#{glibdir}"/library/tests/*.h /usr/local/jamoma/includes`
+    `cp "#{glibdir}"/extensions/TimePluginLib.h /usr/local/jamoma/includes`
     
     # Create alias
-    `sudo ln -s /usr/local/jamoma/extensions/Automation.ttdylib /usr/local/lib/Automation.ttdylib`
-    `sudo ln -s /usr/local/jamoma/extensions/Condition.ttdylib /usr/local/lib/Condition.ttdylib`
-    `sudo ln -s /usr/local/jamoma/extensions/Interval.ttdylib /usr/local/lib/Interval.ttdylib`
-    `sudo ln -s /usr/local/jamoma/extensions/Scenario.ttdylib /usr/local/lib/Scenario.ttdylib`
+    `ln -s /usr/local/jamoma/extensions/Automation.ttdylib /usr/local/lib/Automation.ttdylib`
+    `ln -s /usr/local/jamoma/extensions/Condition.ttdylib /usr/local/lib/Condition.ttdylib`
+    `ln -s /usr/local/jamoma/extensions/Interval.ttdylib /usr/local/lib/Interval.ttdylib`
+    `ln -s /usr/local/jamoma/extensions/Scenario.ttdylib /usr/local/lib/Scenario.ttdylib`
     
 end
 
