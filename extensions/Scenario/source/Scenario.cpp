b33bbe64ead6ee800bcaf2249806749849ef62c8
theod <theod@gmea.net>
2013-10-09 10:42:46 +0200

    Writing and reading root scenario start and end events

--- extensions/Scenario/source/Scenario.cpp
+++ extensions/Scenario/source/Scenario.cpp
@@ -197,19 +197,13 @@ TTErr Scenario::WriteAsXml(const TTValue& inputValue, TTValue& outputValue)
     // if the scenario is not handled by a upper scenario
     if (mContainer == NULL) {
         
+        TTValue     v;
+        TTString    s;
+        
         // Start a Scenario node
         xmlTextWriterStartElement((xmlTextWriterPtr)aXmlHandler->mWriter, BAD_CAST "Scenario");
         
         writeTimeProcessAsXml(aXmlHandler, this);
-        
-        TTValue     v;
-        TTString    s;
-        
-        // Write the end date (e.g. root scenario duration)
-        this->getAttributeValue(TTSymbol("endDate"), v);
-        v.toString();
-        s = TTString(v[0]);
-        xmlTextWriterWriteAttribute((xmlTextWriterPtr)aXmlHandler->mWriter, BAD_CAST "endDate", BAD_CAST s.data());
         
         // Write the view zoom
         v = mViewZoom;
@@ -222,6 +216,38 @@ TTErr Scenario::WriteAsXml(const TTValue& inputValue, TTValue& outputValue)
         v.toString();
         s = TTString(v[0]);
         xmlTextWriterWriteAttribute((xmlTextWriterPtr)aXmlHandler->mWriter, BAD_CAST "viewPosition", BAD_CAST s.data());
+        
+        // Write the start event
+        {
+            xmlTextWriterStartElement((xmlTextWriterPtr)aXmlHandler->mWriter, BAD_CAST "startEvent");
+            
+            // Write the name
+            xmlTextWriterWriteAttribute((xmlTextWriterPtr)aXmlHandler->mWriter, BAD_CAST "name", BAD_CAST kTTSym_start.c_str());
+            
+            // Pass the xml handler to the event to fill his attribute
+            v = TTObjectBasePtr(getStartEvent());
+            aXmlHandler->setAttributeValue(kTTSym_object, v);
+            aXmlHandler->sendMessage(kTTSym_Write);
+            
+            // Close the event node
+            xmlTextWriterEndElement((xmlTextWriterPtr)aXmlHandler->mWriter);
+        }
+        
+        // Write the end event
+        {
+            xmlTextWriterStartElement((xmlTextWriterPtr)aXmlHandler->mWriter, BAD_CAST "endEvent");
+            
+            // Write the name
+            xmlTextWriterWriteAttribute((xmlTextWriterPtr)aXmlHandler->mWriter, BAD_CAST "name", BAD_CAST kTTSym_end.c_str());
+            
+            // Pass the xml handler to the event to fill his attribute
+            v = TTObjectBasePtr(getEndEvent());
+            aXmlHandler->setAttributeValue(kTTSym_object, v);
+            aXmlHandler->sendMessage(kTTSym_Write);
+            
+            // Close the event node
+            xmlTextWriterEndElement((xmlTextWriterPtr)aXmlHandler->mWriter);
+        }
     }
     
     // Write all the time events
@@ -327,18 +353,6 @@ TTErr Scenario::ReadFromXml(const TTValue& inputValue, TTValue& outputValue)
                 }
             }
             
-            // Get the scenario end date (e.g. root scenario duration)
-            if (!aXmlHandler->getXmlAttribute(kTTSym_endDate, v, NO)) {
-                
-                if (v.size() == 1) {
-                    
-                    if (v[0].type() == kTypeUInt32) {
-                        
-                         this->setAttributeValue(TTSymbol("endDate"), v);
-                    }
-                }
-            }
-            
             // Get the scenario color
             if (!aXmlHandler->getXmlAttribute(kTTSym_color, v, NO)) {
                 
@@ -350,6 +364,48 @@ TTErr Scenario::ReadFromXml(const TTValue& inputValue, TTValue& outputValue)
                     }
                 }
             }
+        }
+        
+        // Start Event node
+        if (aXmlHandler->mXmlNodeName == TTSymbol("startEvent")) {
+            
+            if (aXmlHandler->mXmlNodeStart) {
+                
+                // Get the date
+                if (!aXmlHandler->getXmlAttribute(kTTSym_date, v, NO))
+                    getStartEvent()->setAttributeValue(kTTSym_date, v);
+
+                // Get the name
+                if (!aXmlHandler->getXmlAttribute(kTTSym_name, v, YES))
+                    getStartEvent()->setAttributeValue(kTTSym_name, v);
+ 
+            }
+            
+            // Pass the xml handler to the start event to fill his attribute
+            v = TTObjectBasePtr(getStartEvent());
+            aXmlHandler->setAttributeValue(kTTSym_object, v);
+            aXmlHandler->sendMessage(kTTSym_Read);
+        }
+        
+        // End Event node
+        if (aXmlHandler->mXmlNodeName == TTSymbol("endEvent")) {
+            
+            if (aXmlHandler->mXmlNodeStart) {
+                
+                // Get the date
+                if (!aXmlHandler->getXmlAttribute(kTTSym_date, v, NO))
+                    getEndEvent()->setAttributeValue(kTTSym_date, v);
+                
+                // Get the name
+                if (!aXmlHandler->getXmlAttribute(kTTSym_name, v, YES))
+                    getEndEvent()->setAttributeValue(kTTSym_name, v);
+                
+            }
+            
+            // Pass the xml handler to the end event to fill his attribute
+            v = TTObjectBasePtr(getEndEvent());
+            aXmlHandler->setAttributeValue(kTTSym_object, v);
+            aXmlHandler->sendMessage(kTTSym_Read);
         }
     }
     
