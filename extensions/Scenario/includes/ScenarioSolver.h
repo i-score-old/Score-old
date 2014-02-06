c79592eb3afa27bf082cf74e09dc723843799ed0
Jean-Michaël Celerier <jcelerier@enseirb-matmeca.fr>
2014-02-06 15:34:02 +0100

    work in order to get Score to build under Linux

--- extensions/Scenario/includes/ScenarioSolver.h
+++ extensions/Scenario/includes/ScenarioSolver.h
@@ -18,7 +18,7 @@
 #ifndef __SCENARIO_SOLVER_H__
 #define __SCENARIO_SOLVER_H__
 
-#include "Solver.hpp"
+#include "solver.hpp"
 #include "TTTimeEvent.h"
 
 typedef Solver* SolverPtr;
@@ -74,23 +74,23 @@ enum SolverRelationType { EQ_RELATION = 0, NQ_RELATION = 1, LQ_RELATION = 2, LE_
 class SolverVariable
 {
 public:
-    
+
     TTTimeEventPtr    event;
-    
+
     SolverPtr       solver;
     int             dateID;
     int             rangeID;
-    
+
     SolverVariable(SolverPtr aSolver, TTTimeEventPtr anEvent, SolverValue max);
-    
+
     ~SolverVariable();
-    
+
     /** Get the variable value from the solver */
     SolverValue get();
-    
+
     /** Set the range bounds of the variable */
     void limit(SolverValue min, SolverValue max);
-    
+
     /** Update the variable value from the solver */
     void update();
 };
@@ -105,26 +105,26 @@ typedef SolverVariable* SolverVariablePtr;
 class SolverConstraint
 {
 public:
-    
+
     SolverPtr           solver;
     int                 ID;
-    
+
     SolverVariablePtr   startVariable;
     SolverVariablePtr   endVariable;
-    
+
     SolverConstraint(SolverPtr aSolver, SolverVariablePtr variableA, SolverVariablePtr variableB, SolverValue durationMin, SolverValue durationMax, SolverValue max);
-    
+
     ~SolverConstraint();
-    
+
     /** Move a constraint into the solver (then each variable needs to be updated)
-     
+
      @newStart              a new start date
      @newEnd                a new end date
      @return                an error code if movement fails */
     SolverError move(SolverValue newStart, SolverValue newEnd);
-    
+
     /** Change duration bounds of the constraint (then each variable needs to be updated)
-     
+
      @newStart              a new durationMin
      @newEnd                a new durationMax
      @return                an error code if update fails */
@@ -141,27 +141,27 @@ typedef SolverConstraint* SolverConstraintPtr;
 class SolverRelation
 {
 public:
-    
+
     SolverPtr           solver;
     int                 minBoundID;
     int                 maxBoundID;
-    
+
     SolverVariablePtr   startVariable;
     SolverVariablePtr   endVariable;
-    
+
     SolverRelation(SolverPtr aSolver, SolverVariablePtr variableA, SolverVariablePtr variableB, SolverValue durationMin=0, SolverValue durationMax=0);
-    
+
     ~SolverRelation();
-    
+
     /** Move a constraint into the solver (then each variable needs to be updated)
-     
+
      @newStart              a new start date
      @newEnd                a new end date
      @return                an error code movement fails */
     SolverError move(SolverValue newStart, SolverValue newEnd);
-    
+
     /** Change duration bounds of the relation (then each variable needs to be updated)
-     
+
      @newStart              a new durationMin
      @newEnd                a new durationMax
      @return                an error code if update fails */
