b131ebcaa0f3224b5ba7c73a5f2519ea0a7b2cfc
theod <theod@gmea.net>
2014-06-10 15:33:27 +0200

    Revert "Removing date and position notification now an observer can know when a time process starts and ends and so ask those information at its favorite rate"
    This reverts commit 3a5e55dfd36a2cc399f8afb14711e120494c89ae.

--- library/source/TTTimeProcess.cpp
+++ library/source/TTTimeProcess.cpp
@@ -706,6 +706,18 @@ void TTTimeProcessSchedulerCallback(TTPtr object, TTFloat64 position, TTFloat64 
     TTValue             none;
     
     // use the specific process method
-    if (aTimeProcess->mRunning)
+    if (aTimeProcess->mRunning) {
+        
         aTimeProcess->Process(TTValue(position, date), none);
+        
+        // notify position observers
+        TTAttributePtr	positionAttribute;
+        aTimeProcess->findAttribute("position", &positionAttribute);
+        positionAttribute->sendNotification(kTTSym_notify, position);
+        
+        // notify date observers
+        TTAttributePtr	dateAttribute;
+        aTimeProcess->findAttribute("date", &dateAttribute);
+        dateAttribute->sendNotification(kTTSym_notify, date);
+    }
 }
